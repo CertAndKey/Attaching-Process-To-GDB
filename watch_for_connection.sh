f545c439782bb354a0c545482effe3bbea3830c5
CertAndKey <38504609+CertAndKey@users.noreply.github.com>
2023-10-09 14:27:20 -0400

    Update watch_for_connection.sh

--- watch_for_connection.sh
+++ watch_for_connection.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-#This script watches netstat and waits for a connection to be established with the IPs listed in IPs
+#This script finds connections with the IPs listed in IPs and prints the PID and process name when the connection is found
 
 IPs=("<ip1>" "<ip2>" "<ip3>")
 
-while true; do  
-	for ip in "${IPs[@]}"; do    
-		netstat -an | grep "${ip}" | grep -E "ESTABLISHED|SYN_SENT" && echo "Connection detected to ${ip}"  
-	done  
-	sleep 0.1
+while true; do
+  for ip in "${IPs[@]}"; do
+    netstat -anp | grep "${ip}" | grep -E "ESTABLISHED|SYN_SENT" | awk -v OFS=" " '{print $5, $7}' | while read -r ip_address pid_info; do
+      pid="${pid_info%/*}"
+      executable_path=$(readlink -f /proc/"${pid}"/exe)
+      echo "Connection detected to ${ip_address} with PID: ${pid} (Executable: ${executable_path})"
+    done
+  done
+  sleep 0.1
 done
